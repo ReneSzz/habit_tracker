4f242567804ca7e714bca47487a3b6c057f64363
ReneSzz <renesuar99@gmail.com>
2025-02-22 00:32:20 -0500

    gitignore

--- src/app/lib/firebaseConfig.js
+++ src/app/lib/firebaseConfig.js
@@ -1,23 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA6xFkrbxhAgUH4Mq6F8dYDtt4FKztFqQA",
-  authDomain: "habittracker-d7a6d.firebaseapp.com",
-  projectId: "habittracker-d7a6d",
-  storageBucket: "habittracker-d7a6d.firebasestorage.app",
-  messagingSenderId: "185526683860",
-  appId: "1:185526683860:web:5b0357df900b7de0e41341",
-  measurementId: "G-J5FRZH4TH2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
