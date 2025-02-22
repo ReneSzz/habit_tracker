a50c7fd2f01fca77f11d679fc0235e5874eff38d
ReneSzz <renesuar99@gmail.com>
2025-02-22 00:24:06 -0500

    Created header and background colour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
 
 export default function Home() {
   return (
-    <>
-    <h1> Hello World</h1></>
+    <AppBar position="static">
+    <Toolbar>
+      <Typography variant="h6" sx={{ flexGrow: 1 }}>
+        Habit Tracker
+      </Typography>
+      <Button color="inherit">Login</Button>
+    </Toolbar>
+  </AppBar>
   );
 }
